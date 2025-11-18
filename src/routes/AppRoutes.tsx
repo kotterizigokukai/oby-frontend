6299b4e8384c9157aaae59cab0886e12cae190cc
junichi.hattori <junhat6@gmail.com>
2025-11-18 09:24:35 +0900

    プロフィール閲覧画面プロフィール編集画面 #36

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -3,6 +3,9 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from '@/pages/LoginPage';
 import RegisterPage from '@/pages/RegisterPage';
 import CallbackPage from '@/pages/CallbackPage';
+import { ProfilePage } from '@/pages/ProfilePage';
+import { ProfileEditPage } from '@/pages/ProfileEditPage';
+import ProtectedRoute from '@/components/auth/ProtectedRoute';
 
 const AppRoutes: React.FC = () => {
   return (
@@ -11,6 +14,22 @@ const AppRoutes: React.FC = () => {
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/auth/callback" element={<CallbackPage />} />
+      <Route
+        path="/profile"
+        element={
+          <ProtectedRoute>
+            <ProfilePage />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/profile/edit"
+        element={
+          <ProtectedRoute>
+            <ProfileEditPage />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
   );
 };
