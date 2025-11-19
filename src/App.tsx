76797732fa7a4c294c856cc2b96c509b1434d15c
junichi.hattori <junhat6@gmail.com>
2025-11-19 23:28:42 +0900

    ヘッダーをApp.tsxで呼び出すように変更 #40

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { AuthProvider } from './contexts/AuthContext';
 import AppRoutes from './routes/AppRoutes';
+import Header from './components/layout/Header';
 
 // React Query クライアントの作成
 const queryClient = new QueryClient({
@@ -20,7 +21,10 @@ function App() {
         <Router>
           <AuthProvider>
             <div className="flex-1 flex flex-col">
-              <AppRoutes />
+              <Header />
+              <main className="flex-1">
+                <AppRoutes />
+              </main>
             </div>
           </AuthProvider>
         </Router>
